ce79ae089f7f562f7b5a577637721c1f9ed48fe7
Darren Shepherd <darren@acorn.io>
2024-02-02 22:13:12 -0700

    Add sys.http.get and sys.http.post

--- pkg/builtin/builtin.go
+++ pkg/builtin/builtin.go
@@ -2,11 +2,12 @@ package builtin
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
-	"unicode/utf8"
+	"strings"
 
 	"github.com/acorn-io/gptscript/pkg/types"
 )
@@ -30,10 +31,7 @@ var Tools = map[string]types.Tool{
 				return "", err
 			}
 
-			if utf8.Valid(data) {
-				return string(data), nil
-			}
-			return base64.StdEncoding.EncodeToString(data), nil
+			return string(data), nil
 		},
 	},
 	"sys.write": {
@@ -55,6 +53,74 @@ var Tools = map[string]types.Tool{
 			log.Debugf(msg)
 
 			return "", os.WriteFile(params.Filename, data, 0644)
+		},
+	},
+	"sys.http.get": {
+		Description: "Download the contents of a http or https URL",
+		Arguments: types.ObjectSchema(
+			"url", "The URL to download"),
+		BuiltinFunc: func(ctx context.Context, env []string, input string) (string, error) {
+			var params struct {
+				URL string `json:"url,omitempty"`
+			}
+			if err := json.Unmarshal([]byte(input), &params); err != nil {
+				return "", err
+			}
+
+			log.Debugf("http get %s", params.URL)
+			resp, err := http.Get(params.URL)
+			if err != nil {
+				return "", err
+			}
+			defer resp.Body.Close()
+			if resp.StatusCode != http.StatusOK {
+				return "", fmt.Errorf("failed to download %s: %s", params.URL, resp.Status)
+			}
+
+			data, err := io.ReadAll(resp.Body)
+			if err != nil {
+				return "", err
+			}
+
+			return string(data), nil
+		},
+	},
+	"sys.http.post": {
+		Description: "Write contents to a http or https URL using the POST method",
+		Arguments: types.ObjectSchema(
+			"url", "The URL to POST to",
+			"content", "The content to POST",
+			"contentType", "The \"content type\" of the content such as application/json or text/plain"),
+		BuiltinFunc: func(ctx context.Context, env []string, input string) (string, error) {
+			var params struct {
+				URL         string `json:"url,omitempty"`
+				Content     string `json:"content,omitempty"`
+				ContentType string `json:"contentType,omitempty"`
+			}
+			if err := json.Unmarshal([]byte(input), &params); err != nil {
+				return "", err
+			}
+
+			req, err := http.NewRequestWithContext(ctx, http.MethodPost, params.URL, strings.NewReader(params.Content))
+			if err != nil {
+				return "", err
+			}
+			if params.ContentType != "" {
+				req.Header.Set("Content-Type", params.ContentType)
+			}
+
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				return "", err
+			}
+			defer resp.Body.Close()
+
+			_, _ = io.ReadAll(resp.Body)
+			if resp.StatusCode > 399 {
+				return "", fmt.Errorf("failed to post %s: %s", params.URL, resp.Status)
+			}
+
+			return fmt.Sprintf("Wrote %d to %s", len([]byte(params.Content)), params.URL), nil
 		},
 	},
 }
