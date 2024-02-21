90e52e4c24982221166eb605cc732d304eb8fa90
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-02-21 03:02:35 -0500

    enhance: synchronize file manipulation for built-ins
    Use named locks to prevent concurrent writes to files manipulated by
    built-in tools. Locks are named after the file they protect. Lock
    acquisition has no guaranteed order. A more complex solution should
    be considered if a guaranteed order is desired; e.g. FIFO.
    
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- pkg/builtin/builtin.go
+++ pkg/builtin/builtin.go
@@ -15,6 +15,7 @@ import (
 	"sort"
 	"strings"
 
+	"github.com/BurntSushi/locker"
 	"github.com/gptscript-ai/gptscript/pkg/types"
 	"github.com/jaytaylor/html2text"
 )
@@ -242,6 +243,10 @@ func SysRead(ctx context.Context, env []string, input string) (string, error) {
 		return "", err
 	}
 
+	// Lock the file to prevent concurrent writes from other tool calls.
+	locker.RLock(params.Filename)
+	defer locker.RUnlock(params.Filename)
+
 	log.Debugf("Reading file %s", params.Filename)
 	data, err := os.ReadFile(params.Filename)
 	if err != nil {
@@ -260,6 +265,10 @@ func SysWrite(ctx context.Context, env []string, input string) (string, error) {
 		return "", err
 	}
 
+	// Lock the file to prevent concurrent writes from other tool calls.
+	locker.Lock(params.Filename)
+	defer locker.Unlock(params.Filename)
+
 	data := []byte(params.Content)
 	msg := fmt.Sprintf("Wrote %d bytes to file %s", len(data), params.Filename)
 	log.Debugf(msg)
@@ -275,6 +284,10 @@ func SysAppend(ctx context.Context, env []string, input string) (string, error) 
 	if err := json.Unmarshal([]byte(input), &params); err != nil {
 		return "", err
 	}
+
+	// Lock the file to prevent concurrent writes from other tool calls.
+	locker.Lock(params.Filename)
+	defer locker.Unlock(params.Filename)
 
 	f, err := os.OpenFile(params.Filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0644)
 	if err != nil {
@@ -409,6 +422,10 @@ func SysRemove(ctx context.Context, env []string, input string) (string, error) 
 	if err := json.Unmarshal([]byte(input), &params); err != nil {
 		return "", err
 	}
+
+	// Lock the file to prevent concurrent writes from other tool calls.
+	locker.Lock(params.Location)
+	defer locker.Unlock(params.Location)
 
 	return fmt.Sprintf("Removed file: %s", params.Location), os.Remove(params.Location)
 }
