32c9f53f932fc76e831467b50899866616d1f42b
Luc Juggery <luc.juggery@gmail.com>
2024-03-08 11:19:19 +0100

    add example to get sentiments of a tweet

---
+++ examples/sentiments/main.py
@@ -0,0 +1,21 @@
+import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+# Setup WebDriver options
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+options.add_argument("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
+driver = webdriver.Chrome(options=options)
+
+# Navigate to the page and get field
+driver.get(os.environ["url"])
+try:
+    element = WebDriverWait(driver, 50).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "div[data-testid='tweetText'] > span"))
+    )
+    print(element.text)
+finally:
+    driver.quit()
