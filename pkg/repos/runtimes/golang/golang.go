754ed30b630561ff09cbe6061294ea0e7023993c
Daishan Peng <daishan@acorn.io>
2024-03-13 15:51:11 -0700

    Fix: Use subPath as root dir when invoking runtime
    Signed-off-by: Daishan Peng <daishan@acorn.io>

--- pkg/repos/runtimes/golang/golang.go
+++ pkg/repos/runtimes/golang/golang.go
@@ -77,7 +77,7 @@ func stripGo(env []string) (result []string) {
 
 func (r *Runtime) runBuild(ctx context.Context, toolSource, binDir string, env []string) error {
 	log.Infof("Running go build in %s", toolSource)
-	cmd := debugcmd.New(ctx, filepath.Join(binDir, "go"), "build", "-o", "bin/gptscript-go-tool")
+	cmd := debugcmd.New(ctx, filepath.Join(binDir, "go"), "build", "-buildvcs=false", "-o", "bin/gptscript-go-tool")
 	cmd.Env = stripGo(env)
 	cmd.Dir = toolSource
 	return cmd.Run()
