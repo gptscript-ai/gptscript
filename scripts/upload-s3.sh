bb00ed038da46b39d7be0d0ed44231b56fbe68ea
Grant Linville <grant@acorn.io>
2024-03-13 22:57:31 -0400

    chore: upload s3: remove files containing search results (#151)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- scripts/upload-s3.sh
+++ scripts/upload-s3.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# Remove any files that could contain search results from a search API
+grep -c "URL:" "${TMPDIR}"/gptscript-state* | grep -v ":0" | cut -d ':' -f1 | xargs rm
+
 find $TMPDIR -maxdepth 1 -type f -name 'gptscript-state*' -mtime -7 | xargs -I{} aws s3 cp {} s3://gptscript-state/
