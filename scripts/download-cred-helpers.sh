ff39d4490d958ec315d1faae25b80b3512849389
Grant Linville <grant@acorn.io>
2024-04-25 10:33:35 -0400

    fix: add credential helpers to goreleaser archives (#278)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- scripts/download-cred-helpers.sh
+++ scripts/download-cred-helpers.sh
@@ -7,14 +7,18 @@ set -euo pipefail
 GPTSCRIPT_CRED_HELPERS_VERSION="v0.1.0"
 BINARY_DIR="binaries"
 
-mkdir -p "$BINARY_DIR"
+mkdir -p $BINARY_DIR/darwin
+mkdir -p $BINARY_DIR/windows/{amd64,arm64}
 cd "$BINARY_DIR"
 
 wget -O gptscript-credential-osxkeychain "https://github.com/gptscript-ai/gptscript-credential-helpers/releases/download/${GPTSCRIPT_CRED_HELPERS_VERSION}/gptscript-credential-osxkeychain"
 chmod +x gptscript-credential-osxkeychain
+mv gptscript-credential-osxkeychain darwin/gptscript-credential-osxkeychain
 
 wget -O gptscript-credential-wincred-amd64.exe "https://github.com/gptscript-ai/gptscript-credential-helpers/releases/download/${GPTSCRIPT_CRED_HELPERS_VERSION}/gptscript-credential-wincred-${GPTSCRIPT_CRED_HELPERS_VERSION}.windows-amd64.exe"
 chmod +x gptscript-credential-wincred-amd64.exe
+mv gptscript-credential-wincred-amd64.exe windows/amd64/gptscript-credential-wincred.exe
 
 wget -O gptscript-credential-wincred-arm64.exe "https://github.com/gptscript-ai/gptscript-credential-helpers/releases/download/${GPTSCRIPT_CRED_HELPERS_VERSION}/gptscript-credential-wincred-${GPTSCRIPT_CRED_HELPERS_VERSION}.windows-arm64.exe"
 chmod +x gptscript-credential-wincred-arm64.exe
+mv gptscript-credential-wincred-arm64.exe windows/arm64/gptscript-credential-wincred.exe
