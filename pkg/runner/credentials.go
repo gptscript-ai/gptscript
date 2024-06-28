ba7fb94506c7056c89cbc308ea57634c493139f1
Grant Linville <grant@acorn.io>
2024-06-28 10:22:33 -0400

    chore: remove third override syntax
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/runner/credentials.go
+++ pkg/runner/credentials.go
@@ -7,10 +7,9 @@ import (
 )
 
 // parseCredentialOverrides parses a string of credential overrides that the user provided as a command line arg.
-// The format of credential overrides can be one of three things:
+// The format of credential overrides can be one of two things:
 // cred1:ENV1,ENV2;cred2:ENV1,ENV2 (direct mapping of environment variables)
 // cred1:ENV1=VALUE1,ENV2=VALUE2;cred2:ENV1=VALUE1,ENV2=VALUE2 (key-value pairs)
-// cred1:ENV1->OTHER_ENV1,ENV2->OTHER_ENV2;cred2:ENV1->OTHER_ENV1,ENV2->OTHER_ENV2 (mapping to other environment variables)
 //
 // This function turns it into a map[string]map[string]string like this:
 //
@@ -36,15 +35,8 @@ func parseCredentialOverrides(override string) (map[string]map[string]string, er
 		for _, env := range strings.Split(envs, ",") {
 			key, value, found := strings.Cut(env, "=")
 			if !found {
-				var envVar string
-				key, envVar, found = strings.Cut(env, "->")
-				if found {
-					// User did a mapping of key -> other env var, so look up the value.
-					value = os.Getenv(envVar)
-				} else {
-					// User just passed an env var name as the key, so look up the value.
-					value = os.Getenv(key)
-				}
+				// User just passed an env var name as the key, so look up the value.
+				value = os.Getenv(key)
 			}
 			envMap[key] = value
 		}
---
+++ pkg/runner/credentials_test.go
@@ -0,0 +1,128 @@
+package runner
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestParseCredentialOverrides(t *testing.T) {
+	cases := []struct {
+		name      string
+		envs      map[string]string
+		in        string
+		out       map[string]map[string]string
+		expectErr bool
+	}{
+		{
+			name:      "empty",
+			in:        "",
+			expectErr: true,
+		},
+		{
+			name: "single cred, single env",
+			envs: map[string]string{
+				"ENV1": "VALUE1",
+			},
+			in: "cred1:ENV1",
+			out: map[string]map[string]string{
+				"cred1": {
+					"ENV1": "VALUE1",
+				},
+			},
+		},
+		{
+			name: "single cred, multiple envs",
+			envs: map[string]string{
+				"ENV1": "VALUE1",
+				"ENV2": "VALUE2",
+			},
+			in: "cred1:ENV1,ENV2",
+			out: map[string]map[string]string{
+				"cred1": {
+					"ENV1": "VALUE1",
+					"ENV2": "VALUE2",
+				},
+			},
+		},
+		{
+			name: "single cred, key value pairs",
+			envs: map[string]string{
+				"ENV1": "VALUE1",
+				"ENV2": "VALUE2",
+			},
+			in: "cred1:ENV1=OTHERVALUE1,ENV2=OTHERVALUE2",
+			out: map[string]map[string]string{
+				"cred1": {
+					"ENV1": "OTHERVALUE1",
+					"ENV2": "OTHERVALUE2",
+				},
+			},
+		},
+		{
+			name: "multiple creds, multiple envs",
+			envs: map[string]string{
+				"ENV1": "VALUE1",
+				"ENV2": "VALUE2",
+			},
+			in: "cred1:ENV1,ENV2;cred2:ENV1,ENV2",
+			out: map[string]map[string]string{
+				"cred1": {
+					"ENV1": "VALUE1",
+					"ENV2": "VALUE2",
+				},
+				"cred2": {
+					"ENV1": "VALUE1",
+					"ENV2": "VALUE2",
+				},
+			},
+		},
+		{
+			name: "multiple creds, key value pairs",
+			envs: map[string]string{
+				"ENV1": "VALUE1",
+				"ENV2": "VALUE2",
+			},
+			in: "cred1:ENV1=OTHERVALUE1,ENV2=OTHERVALUE2;cred2:ENV1=OTHERVALUE3,ENV2=OTHERVALUE4",
+			out: map[string]map[string]string{
+				"cred1": {
+					"ENV1": "OTHERVALUE1",
+					"ENV2": "OTHERVALUE2",
+				},
+				"cred2": {
+					"ENV1": "OTHERVALUE3",
+					"ENV2": "OTHERVALUE4",
+				},
+			},
+		},
+		{
+			name:      "invalid format",
+			in:        "cred1=ENV1,ENV2",
+			expectErr: true,
+		},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.name, func(t *testing.T) {
+			envs := tc.envs
+			if envs == nil {
+				envs = map[string]string{}
+			}
+
+			for k, v := range envs {
+				_ = os.Setenv(k, v)
+			}
+
+			out, err := parseCredentialOverrides(tc.in)
+			if tc.expectErr {
+				require.Error(t, err)
+				return
+			}
+			require.NoError(t, err)
+
+			require.Equal(t, len(tc.out), len(out), "expected %d creds, but got %d", len(tc.out), len(out))
+			require.Equal(t, tc.out, out, "expected output %v, but got %v", tc.out, out)
+		})
+	}
+}
