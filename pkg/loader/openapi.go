2f70c1e395313f5e5dd8bb2957314f10befdc8bf
John Engelman <john.engelman@target.com>
2024-07-25 11:53:24 -0500

    fix: Append all credentials for OpenAPI security infos

--- pkg/loader/openapi.go
+++ pkg/loader/openapi.go
@@ -305,7 +305,7 @@ func getOpenAPITools(t *openapi3.T, defaultHost, source, targetToolName string) 
 					if err != nil {
 						return nil, fmt.Errorf("failed to parse operation server URL: %w", err)
 					}
-					tool.Credentials = info.GetCredentialToolStrings(operationServerURL.Hostname())
+					tool.Credentials = append(tool.Credentials, info.GetCredentialToolStrings(operationServerURL.Hostname())...)
 				}
 			}
 
