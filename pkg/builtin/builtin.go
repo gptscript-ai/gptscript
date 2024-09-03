eaaf0cdb72cae37f84ce600bb49ae1f49fc86be9
Grant Linville <grant@acorn.io>
2024-09-03 12:28:49 -0400

    fix: sys.read: never read files that contain a null byte (#837)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- pkg/builtin/builtin.go
+++ pkg/builtin/builtin.go
@@ -489,6 +489,12 @@ func SysRead(_ context.Context, _ []string, input string, _ chan<- string) (stri
 	if len(data) == 0 {
 		return fmt.Sprintf("The file %s has no contents", params.Filename), nil
 	}
+
+	// Assume the file is not text if it contains a null byte
+	if bytes.IndexByte(data, 0) != -1 {
+		return fmt.Sprintf("The file %s cannot be read because it is not a plaintext file", params.Filename), nil
+	}
+
 	return string(data), nil
 }
 
