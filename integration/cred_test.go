d2442f4a312f3b8774669ccbc43109a84d9030af
Grant Linville <grant@acorn.io>
2024-09-16 18:26:35 -0400

    add integration test
    Signed-off-by: Grant Linville <grant@acorn.io>

--- integration/cred_test.go
+++ integration/cred_test.go
@@ -45,3 +45,57 @@ func TestCredentialExpirationEnv(t *testing.T) {
 		}
 	}
 }
+
+// TestStackedCredentialContexts tests creating, using, listing, showing, and deleting credentials when there are multiple contexts.
+func TestStackedCredentialContexts(t *testing.T) {
+	// First, test credential creation. We will create a credential called testcred in two different contexts called one and two.
+	_, err := RunScript("scripts/cred_stacked.gpt", "--sub-tool", "testcred_one", "--credential-context", "one,two")
+	require.NoError(t, err)
+
+	_, err = RunScript("scripts/cred_stacked.gpt", "--sub-tool", "testcred_two", "--credential-context", "two")
+	require.NoError(t, err)
+
+	// Next, we try running the testcred_one tool. It should print the value of "testcred" in whichever context it finds the cred first.
+	out, err := RunScript("scripts/cred_stacked.gpt", "--sub-tool", "testcred_one", "--credential-context", "one,two")
+	require.NoError(t, err)
+	require.Contains(t, out, "one")
+	require.NotContains(t, out, "two")
+
+	out, err = RunScript("scripts/cred_stacked.gpt", "--sub-tool", "testcred_one", "--credential-context", "two,one")
+	require.NoError(t, err)
+	require.Contains(t, out, "two")
+	require.NotContains(t, out, "one")
+
+	// Next, list credentials and specify both contexts. We should get the credential from the first specified context.
+	out, err = GPTScriptExec("--credential-context", "one,two", "cred")
+	require.NoError(t, err)
+	require.Contains(t, out, "one")
+	require.NotContains(t, out, "two")
+
+	out, err = GPTScriptExec("--credential-context", "two,one", "cred")
+	require.NoError(t, err)
+	require.Contains(t, out, "two")
+	require.NotContains(t, out, "one")
+
+	// Next, try showing the credentials.
+	out, err = GPTScriptExec("--credential-context", "one,two", "cred", "show", "testcred")
+	require.NoError(t, err)
+	require.Contains(t, out, "one")
+	require.NotContains(t, out, "two")
+
+	out, err = GPTScriptExec("--credential-context", "two,one", "cred", "show", "testcred")
+	require.NoError(t, err)
+	require.Contains(t, out, "two")
+	require.NotContains(t, out, "one")
+
+	// Make sure we get an error if we try to delete a credential with multiple contexts specified.
+	_, err = GPTScriptExec("--credential-context", "one,two", "cred", "delete", "testcred")
+	require.Error(t, err)
+
+	// Now actually delete the credentials.
+	_, err = GPTScriptExec("--credential-context", "one", "cred", "delete", "testcred")
+	require.NoError(t, err)
+
+	_, err = GPTScriptExec("--credential-context", "two", "cred", "delete", "testcred")
+	require.NoError(t, err)
+}
