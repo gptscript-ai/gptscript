d70f9193827e01becd201df383e380102c504b55
Darren Shepherd <darren@acorn.io>
2024-10-14 10:10:36 -0700

    chore: add ability to pass args to input/output filters

--- pkg/tests/runner2_test.go
+++ pkg/tests/runner2_test.go
@@ -2,10 +2,12 @@ package tests
 
 import (
 	"context"
+	"encoding/json"
 	"testing"
 
 	"github.com/gptscript-ai/gptscript/pkg/loader"
 	"github.com/gptscript-ai/gptscript/pkg/tests/tester"
+	"github.com/hexops/autogold/v2"
 	"github.com/stretchr/testify/require"
 )
 
@@ -110,4 +112,93 @@ echo '{"env": {"CRED2": "that also worked"}}'
 
 	resp, err := r.Chat(context.Background(), nil, prg, nil, "")
 	r.AssertStep(t, resp, err)
+}
+
+func TestFilterArgs(t *testing.T) {
+	r := tester.NewRunner(t)
+	prg, err := loader.ProgramFromSource(context.Background(), `
+inputfilters: input with ${Foo}
+inputfilters: input with foo
+inputfilters: input with *
+outputfilters: output with *
+outputfilters: output with foo
+outputfilters: output with ${Foo}
+params: Foo: a description
+
+#!/bin/bash
+echo ${FOO}
+
+---
+name: input
+params: notfoo: a description
+
+#!/bin/bash
+echo "${GPTSCRIPT_INPUT}"
+
+---
+name: output
+params: notfoo: a description
+
+#!/bin/bash
+echo "${GPTSCRIPT_INPUT}"
+`, "")
+	require.NoError(t, err)
+
+	resp, err := r.Chat(context.Background(), nil, prg, nil, `{"foo":"baz", "start": true}`)
+	r.AssertStep(t, resp, err)
+
+	data := map[string]any{}
+	err = json.Unmarshal([]byte(resp.Content), &data)
+	require.NoError(t, err)
+
+	autogold.Expect(map[string]interface{}{
+		"chat":         false,
+		"continuation": false,
+		"notfoo":       "baz",
+		"output": `{"chat":false,"continuation":false,"notfoo":"foo","output":"{\"chat\":false,\"continuation\":false,\"foo\":\"baz\",\"input\":\"{\\\"foo\\\":\\\"baz\\\",\\\"input\\\":\\\"{\\\\\\\"foo\\\\\\\":\\\\\\\"baz\\\\\\\", \\\\\\\"start\\\\\\\": true}\\\",\\\"notfoo\\\":\\\"baz\\\",\\\"start\\\":true}\\n\",\"notfoo\":\"foo\",\"output\":\"baz\\n\",\"start\":true}\n"}
+`,
+	}).Equal(t, data)
+
+	val := data["output"].(string)
+	data = map[string]any{}
+	err = json.Unmarshal([]byte(val), &data)
+	require.NoError(t, err)
+	autogold.Expect(map[string]interface{}{
+		"chat":         false,
+		"continuation": false,
+		"notfoo":       "foo",
+		"output": `{"chat":false,"continuation":false,"foo":"baz","input":"{\"foo\":\"baz\",\"input\":\"{\\\"foo\\\":\\\"baz\\\", \\\"start\\\": true}\",\"notfoo\":\"baz\",\"start\":true}\n","notfoo":"foo","output":"baz\n","start":true}
+`,
+	}).Equal(t, data)
+
+	val = data["output"].(string)
+	data = map[string]any{}
+	err = json.Unmarshal([]byte(val), &data)
+	require.NoError(t, err)
+	autogold.Expect(map[string]interface{}{
+		"chat":         false,
+		"continuation": false,
+		"foo":          "baz", "input": `{"foo":"baz","input":"{\"foo\":\"baz\", \"start\": true}","notfoo":"baz","start":true}
+`,
+		"notfoo": "foo",
+		"output": "baz\n",
+		"start":  true,
+	}).Equal(t, data)
+
+	val = data["input"].(string)
+	data = map[string]any{}
+	err = json.Unmarshal([]byte(val), &data)
+	require.NoError(t, err)
+	autogold.Expect(map[string]interface{}{
+		"foo":    "baz",
+		"input":  `{"foo":"baz", "start": true}`,
+		"notfoo": "baz",
+		"start":  true,
+	}).Equal(t, data)
+
+	val = data["input"].(string)
+	data = map[string]any{}
+	err = json.Unmarshal([]byte(val), &data)
+	require.NoError(t, err)
+	autogold.Expect(map[string]interface{}{"foo": "baz", "start": true}).Equal(t, data)
 }
