468c4f1a3cd98c1ebc0c3672d65a2a4e3318217b
Donnie Adams <donnie@acorn.io>
2025-02-04 09:58:59 -0500

    enhance: add field-level sensitivity for prompts
    Additionally, each field can now also have a description.
    
    This change is made such that all existing tools will work. However,
    existing code will need to be updated to support the new types.
    
    Signed-off-by: Donnie Adams <donnie@acorn.io>

--- pkg/types/prompt.go
+++ pkg/types/prompt.go
@@ -1,5 +1,10 @@
 package types
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 const (
 	PromptURLEnvVar   = "GPTSCRIPT_PROMPT_URL"
 	PromptTokenEnvVar = "GPTSCRIPT_PROMPT_TOKEN"
@@ -7,7 +12,63 @@ const (
 
 type Prompt struct {
 	Message   string            `json:"message,omitempty"`
-	Fields    []string          `json:"fields,omitempty"`
+	Fields    Fields            `json:"fields,omitempty"`
 	Sensitive bool              `json:"sensitive,omitempty"`
 	Metadata  map[string]string `json:"metadata,omitempty"`
+}
+
+type Field struct {
+	Name        string `json:"name,omitempty"`
+	Sensitive   *bool  `json:"sensitive,omitempty"`
+	Description string `json:"description,omitempty"`
+}
+
+type Fields []Field
+
+// UnmarshalJSON will unmarshal the corresponding JSON object for Fields,
+// or a comma-separated strings (for backwards compatibility).
+func (f *Fields) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || f == nil {
+		return nil
+	}
+
+	if b[0] == '[' {
+		var arr []Field
+		if err := json.Unmarshal(b, &arr); err != nil {
+			return err
+		}
+		*f = arr
+		return nil
+	}
+
+	var fields string
+	if err := json.Unmarshal(b, &fields); err != nil {
+		return err
+	}
+
+	if fields != "" {
+		fieldsArr := strings.Split(fields, ",")
+		*f = make([]Field, 0, len(fieldsArr))
+		for _, field := range fieldsArr {
+			*f = append(*f, Field{Name: strings.TrimSpace(field)})
+		}
+	}
+
+	return nil
+}
+
+type field *Field
+
+// UnmarshalJSON will unmarshal the corresponding JSON object for a Field,
+// or a string (for backwards compatibility).
+func (f *Field) UnmarshalJSON(b []byte) error {
+	if len(b) == 0 || f == nil {
+		return nil
+	}
+
+	if b[0] == '{' {
+		return json.Unmarshal(b, field(f))
+	}
+
+	return json.Unmarshal(b, &f.Name)
 }
